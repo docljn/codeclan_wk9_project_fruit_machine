53eb5a05a1756d98a87543a4403bb20631e4ae8c
DocLJN <ljnoble@gmail.com>
2018-01-27 14:24:16 +0000

    Reel has number of stops, image lookup reference, setup reference, held status.

--- app/src/main/java/example/docljn/com/fruitmachine/JavaLogic/Reel.java
+++ app/src/main/java/example/docljn/com/fruitmachine/JavaLogic/Reel.java
@@ -5,13 +5,48 @@ package example.docljn.com.fruitmachine.JavaLogic;
  */
 
 public class Reel {
+    private int imageLookupTable;
     private int numberOfStops;
+    private int setupReference;
+    private boolean held;
 
-    public Reel(int numberOfStops) {
+    public Reel(int numberOfStops, int imageLookupTable, int setupReference) {
+        this.imageLookupTable = imageLookupTable;
         this.numberOfStops = numberOfStops;
+        this.setupReference = setupReference;
+        this.held = false;
     }
 
     public int getNumberOfStops() {
         return this.numberOfStops;
+    }
+
+    public int getImageLookupTable() {
+        return this.imageLookupTable;
+    }
+
+    public int getSetupReference() {
+        return setupReference;
+    }
+
+    public boolean getHeld() {
+        return this.held;
+    }
+
+    public void setNumberOfStops(int numberOfStops) {
+        this.numberOfStops = numberOfStops;
+    }
+
+    public void setImageLookupTable(int imageLookupTable) {
+        this.imageLookupTable = imageLookupTable;
+    }
+
+
+    public void setSetupReference(int setupReference) {
+        this.setupReference = setupReference;
+    }
+
+    public void setHeld(boolean held) {
+        this.held = held;
     }
 }
--- app/src/test/java/example/docljn/com/fruitmachine/ReelTest.java
+++ app/src/test/java/example/docljn/com/fruitmachine/ReelTest.java
@@ -17,11 +17,55 @@ public class ReelTest {
 
     @Before
     public void before(){
-        reel = new Reel(11);
+        reel = new Reel(11, 1, 2);
     }
 
     @Test
     public void getsNumberOfStops(){
         assertEquals(11, reel.getNumberOfStops());
     }
+
+    @Test
+    public void getsImageLookupTableReference(){
+        assertEquals(1, reel.getImageLookupTable());
+    }
+
+    @Test
+    public void getsReelSetupReference(){
+        assertEquals(2, reel.getSetupReference());
+
+    }
+
+    @Test
+    public void getsHeldStatusFalseAtStart(){
+        assertEquals(false, reel.getHeld());
+    }
+
+
+    @Test
+    public void setsNumberOfStops(){
+        reel.setNumberOfStops(9);
+        assertEquals(9, reel.getNumberOfStops());
+    }
+
+
+    @Test
+    public void setsImageLookupTable(){
+        reel.setImageLookupTable(4);
+        assertEquals(4, reel.getImageLookupTable());
+    }
+
+    @Test
+    public void setsHeldStatus(){
+        reel.setHeld(true);
+        assertEquals(true, reel.getHeld());
+    }
+
+    @Test
+    public void setsSetUpReference(){
+        reel.setSetupReference(1);
+        assertEquals(1, reel.getSetupReference());
+    }
+
+    
 }
