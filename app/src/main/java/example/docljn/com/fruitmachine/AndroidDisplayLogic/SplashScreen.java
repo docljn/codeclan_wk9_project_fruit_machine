46f4a6a4339a47bca599fbbce8b646c0103f1e64
DocLJN <ljnoble@gmail.com>
2018-01-30 14:00:01 +0000

    Start of Grid View Experiments

--- app/src/main/java/example/docljn/com/fruitmachine/AndroidDisplayLogic/SplashScreen.java
+++ app/src/main/java/example/docljn/com/fruitmachine/AndroidDisplayLogic/SplashScreen.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.GridLayout;
 import android.widget.ImageView;
 
+import java.util.HashMap;
+
 import example.docljn.com.fruitmachine.JavaLogic.Game;
 import example.docljn.com.fruitmachine.R;
 
@@ -16,6 +18,11 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        HashMap<Integer, Integer> cardImages = new HashMap<>();
+        cardImages.put(1, R.drawable.club);
+        cardImages.put(2, R.drawable.diamond);
+        cardImages.put(4, R.drawable.heart);
+        cardImages.put(10, R.drawable.spade);
 
         Game game = new Game(3);
         // TODO: add buttons to set the number of reels to 3 or 5 on the front page?
@@ -24,14 +31,26 @@ public class SplashScreen extends AppCompatActivity {
 
         // TODO Do I need a layout inflater here?
 
-        LayoutInflater inflater = (LayoutInflater)getSystemService(this.LAYOUT_INFLATER_SERVICE);
-        View view = inflater.inflate(R.layout.fruit_machine_reels_grid, null);
 
         GridLayout gridLayout = (GridLayout) findViewById(R.id.fruitMachineReelLayout);
         gridLayout.setColumnCount(game.getNumberOfReels());
 
-        ImageView top = findViewById(R.id.imageViewTopReelImage);
-        top.setImageResource(R.drawable.club);
 
     }
 }
+
+/*
+CONSIDER THIS FOR DYNAMIC GRID CONTAINER:
+BUT: this won't allow me to set columns of related images?
+gridContainer = new GridLayout(this);
+        gridContainer.setColumnCount(4);
+        YOUR_OWN_VIEW.addView(gridContainer);
+
+        for(int i = 0; i < 16; i++)
+        {
+            ImageView img = new ImageView(this);
+            img.setImageDrawable(this.getResources().getDrawable(R.drawable.ic_launcher));
+
+            gridContainer.addView(img, Math.max(0, gridContainer.getChildCount()));
+        }
+ */
